db5c9cba34d6cbbf879ecad03464d1c0bee6d0e9
Darren Clark <darrenjoelclark@icloud.com>
2021-02-23 08:12:35 -0500

    Add iterm2_shell_integration

--- install.sh
+++ install.sh
@@ -6,6 +6,6 @@ for dir in .config/*; do
   ln -v -s "$PWD/$dir" "$HOME/$dir"
 done
 
-for file in .zshenv; do
+for file in .zshenv .iterm2_shell_integration.zsh; do
   ln -v -s "$PWD/$file" "$HOME/$file"
 done
