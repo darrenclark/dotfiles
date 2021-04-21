4a64f222bb25df4e034f996ee9927b95163e2c10
Darren Clark <darrenjoelclark@icloud.com>
2021-04-20 22:12:37 -0400

    Fix use in devcontainer?

--- install.sh
+++ install.sh
@@ -2,27 +2,25 @@
 
 set -euo pipefail
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+cd $SCRIPT_DIR
+
 git submodule update --init --recursive
 
 # Copy configs
 mkdir -v -p "$HOME/.config"
 
 for dir in .config/*; do
-  ln -h -v -s "$PWD/$dir" "$HOME/$dir" || true
+  ln -n -v -s "$PWD/$dir" "$HOME/$dir" || true
 done
 
 for file in .zshenv .iterm2_shell_integration.zsh .asdf .elixir-ls; do
-  ln -h -v -s "$PWD/$file" "$HOME/$file" || true
+  ln -n -v -s "$PWD/$file" "$HOME/$file" || true
 done
 
 # Copy shell scripts
 mkdir -v -p "$HOME/bin"
 
 for file in bin/*; do
-  ln -h -v -s "$PWD/$file" "$HOME/$file" || true
+  ln -n -v -s "$PWD/$file" "$HOME/$file" || true
 done
-
-if [[ ! -z "${REMOTE_CONTAINERS:-}" ]] || [[ ! -z "${CODESPACES:-}" ]]; then
-  echo "Running in devcontainer, removing default ZSH config..."
-  rm -v ~/.zshrc ~/.zcompdump
-fi
