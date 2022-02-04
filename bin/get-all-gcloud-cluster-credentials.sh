50d35862fa27e1ae6ac84fa10deaab0da3558087
Darren Clark <darrenjoelclark@icloud.com>
2022-02-04 13:23:31 -0500

    update get-all-gcloud-cluster-credentials.s to support region

--- bin/get-all-gcloud-cluster-credentials.sh
+++ bin/get-all-gcloud-cluster-credentials.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 project="$1"
-region="us-central1"
+region="$2"
 
-if [ -z "$project" ]; then
-	echo "usage:  $0 <gcp project>"
+if [ -z "$project" ] || [ -z "$region" ]; then
+	echo "usage:  $0 <gcp project> <gcp region>"
 	exit 1;
 fi
 
-for cluster in $(gcloud container clusters list --project "$project" --format='value(name)'); do
+for cluster in $(gcloud container clusters list --project "$project" --region="$region" --format='value(name)'); do
 	gcloud container clusters get-credentials "$cluster" --project "$project" --region "$region"
 done
