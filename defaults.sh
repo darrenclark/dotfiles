9b65f8d870a18a2286df163f466c55c2d833dfdf
Darren Clark <darrenjoelclark@icloud.com>
2022-06-11 15:26:12 -0400

    Save screenshots to ~/Screenshots

--- defaults.sh
+++ defaults.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# Dock
 defaults write com.apple.dock tilesize -int 36
 defaults write com.apple.dock autohide -bool true
 killall Dock
+
+# Screenshots
+mkdir -p ~/Screenshots
+defaults write com.apple.screencapture location -string "~/Screenshots"
