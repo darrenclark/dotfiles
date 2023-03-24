53c78c0758181d829f78ca7d68d5af0dfe4f5840
Darren Clark <darrenjoelclark@icloud.com>
2023-03-23 21:09:55 -0400

    Tabs / bufferline working

--- .config/nvim/lua/config/keys.lua
+++ .config/nvim/lua/config/keys.lua
@@ -8,3 +8,19 @@ end
 
 -- Enter to clear search highlight
 map("n", "<CR>", ":nohlsearch<CR>", { silent = true })
+
+-- Previous tab (Ctrl-H or Ctrl-J)
+map("n", "<C-H>", ":bp<CR>", { silent = true })
+map("n", "<C-J>", ":bp<CR>", { silent = true })
+map("v", "<C-H>", "<C-C>:bp<CR>", { silent = true })
+map("v", "<C-J>", "<C-C>:bp<CR>", { silent = true })
+map("i", "<C-H>", "<C-C>:bp<CR>", { silent = true })
+map("i", "<C-J>", "<C-C>:bp<CR>", { silent = true })
+
+-- Next tab (Ctrl-K or Ctrl-L)
+map("n", "<C-K>", ":bn<CR>", { silent = true })
+map("n", "<C-L>", ":bn<CR>", { silent = true })
+map("v", "<C-K>", "<C-C>:bn<CR>", { silent = true })
+map("v", "<C-L>", "<C-C>:bn<CR>", { silent = true })
+map("i", "<C-K>", "<C-C>:bn<CR>", { silent = true })
+map("i", "<C-L>", "<C-C>:bn<CR>", { silent = true })
--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -72,6 +72,38 @@ return {
     },
   },
 
+  -- Bufferline (tabs)
+  {
+    "akinsho/bufferline.nvim",
+    event = "VeryLazy",
+    keys = {
+      { "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle pin" },
+      { "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete non-pinned buffers" },
+    },
+    opts = {
+      options = {
+        diagnostics = "nvim_lsp",
+        always_show_bufferline = false,
+        diagnostics_indicator = function(_, _, diag)
+          local icons = require("lazyvim.config").icons.diagnostics
+          local ret = (diag.error and icons.Error .. diag.error .. " " or "")
+            .. (diag.warning and icons.Warn .. diag.warning or "")
+          return vim.trim(ret)
+        end,
+        show_buffer_icons = false,
+        show_buffer_close_icons = false,
+        offsets = {
+          {
+            filetype = "neo-tree",
+            text = "Neo-tree",
+            highlight = "Directory",
+            text_align = "left",
+          },
+        },
+      },
+    },
+  },
+
   -- ???
   { "nvim-lua/plenary.nvim", lazy = true },
 
