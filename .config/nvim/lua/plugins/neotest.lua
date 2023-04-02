c306c4b82a182a5e2d7174ec718d1b2df2c30ea2
Darren Clark <darrenjoelclark@icloud.com>
2023-04-01 21:18:23 -0400

    neotest - setup keybindings & better failed test reporting

--- .config/nvim/lua/plugins/neotest.lua
+++ .config/nvim/lua/plugins/neotest.lua
@@ -9,7 +9,29 @@ return {
       "jfpedroza/neotest-elixir"
     },
     keys = {
-
+      {
+        "<C-P>",
+        function()
+          local run = require("neotest").run
+          local line = vim.api.nvim_win_get_cursor(0)[1]
+          if line == 1 then
+            run.run(vim.fn.expand("%"))
+          else
+            run.run()
+          end
+        end,
+        mode = {"n"},
+        desc = "Run nearest test or whole file"
+      },
+      {
+        "<C-N>",
+        function()
+          local run = require("neotest").run
+          run.run_last()
+        end,
+        mode = {"n"},
+        desc = "Run previous test"
+      },
     },
     opts = {
       diagnostic = {
@@ -21,12 +43,30 @@ return {
         running = "⏺",
         skipped = "-",
       },
-      status = {
-        --signs = false,
+      output = {
+        open_on_run = false,
       },
       quickfix = {
         open = function()
-          require('telescope.builtin').quickfix()
+          local qflist = vim.fn.getqflist()
+          local in_different_file = qflist[1].bufnr ~= vim.api.nvim_get_current_buf()
+
+          if #qflist > 1 or in_different_file then
+            require('telescope.builtin').quickfix({
+              attach_mappings = function(_, map)
+                map({'i','n'}, "<CR>", function(...)
+                  require('telescope.actions').select_default(...)
+                  require("neotest").output.open({enter=true})
+                end)
+
+                return true
+              end
+            })
+          else
+            -- jump to failed test and show output
+            vim.cmd [[:cfirst]]
+            require("neotest").output.open({enter=true})
+          end
         end,
       }
     },
