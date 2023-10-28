919b58370958e9badacbafe2192f02c7140d9bf4
Darren Clark <darrenjoelclark@icloud.com>
2023-10-27 21:32:43 -0400

    Fix telescope, update plugins

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,5 +1,5 @@
 return {
-  'nvim-telescope/telescope.nvim', tag = '0.1.1',
+  'nvim-telescope/telescope.nvim', tag = '0.1.4',
   dependencies = { 'nvim-lua/plenary.nvim' },
   init = function()
     local rg = function(opts)
