9e895134bcea8e1d7de163f4af5274770ac219bc
Darren Clark <darrenjoelclark@icloud.com>
2024-05-24 21:38:46 -0400

    add vim-elixir to fix indent

--- .config/nvim/lua/plugins/elixir-nvim.lua
+++ .config/nvim/lua/plugins/elixir-nvim.lua
@@ -1,26 +1,28 @@
 return {
-  "elixir-tools/elixir-tools.nvim",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "hrsh7th/cmp-nvim-lsp",
-  },
-  ft = { "elixir", "eex", "heex", "surface" },
-  config = function()
-    local elixir = require("elixir")
-    local elixirls = require("elixir.elixirls")
+  {"elixir-editors/vim-elixir"},
+  {
+    "elixir-tools/elixir-tools.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "hrsh7th/cmp-nvim-lsp",
+    },
+    ft = { "elixir", "eex", "heex", "surface" },
+    config = function()
+      local elixir = require("elixir")
+      local elixirls = require("elixir.elixirls")
 
-    elixir.setup {
-      credo = {},
-      capabilities = require("cmp_nvim_lsp").default_capabilities(),
-      settings = elixirls.settings {
-        dialyzerEnabled = false,
-        enableTestLenses = false,
-        fetchDeps = true,
-        suggestSpecs = false,
-      },
-      log_level = vim.lsp.protocol.MessageType.Log,
-      message_level = vim.lsp.protocol.MessageType.Log,
-    }
-  end,
+      elixir.setup {
+        credo = {},
+        capabilities = require("cmp_nvim_lsp").default_capabilities(),
+        settings = elixirls.settings {
+          dialyzerEnabled = false,
+          enableTestLenses = false,
+          fetchDeps = true,
+          suggestSpecs = false,
+        },
+        log_level = vim.lsp.protocol.MessageType.Log,
+        message_level = vim.lsp.protocol.MessageType.Log,
+      }
+    end,
+  }
 }
-
