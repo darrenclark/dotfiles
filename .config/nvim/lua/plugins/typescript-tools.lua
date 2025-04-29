2e3653e2ebda21f88e3d182469262a7981c54cbd
Darren Clark <darrenjoelclark@icloud.com>
2025-04-29 13:00:05 -0400

    Revert "Use project-relative imports in typescript"
    This reverts commit ef1a565d84dee4e9285850edf9498f1d1455a858.

--- .config/nvim/lua/plugins/typescript-tools.lua
+++ .config/nvim/lua/plugins/typescript-tools.lua
@@ -1,11 +1,5 @@
 return {
   "pmizio/typescript-tools.nvim",
   dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-  opts = {
-    settings = {
-      tsserver_file_preferences = {
-        importModuleSpecifierPreference = "project-relative",
-      }
-    }
-  },
+  opts = {},
 }
