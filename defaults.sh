a7a9c5f6fc1b8d5a0869863a3cfefda2b37ee993
Darren Clark <darrenjoelclark@icloud.com>
2026-02-25 17:47:49 -0500

    Fix Mac Dock disappearing after running `install.sh`
    `killall Dock` no longer works well, using `launchctl` instead

--- defaults.sh
+++ defaults.sh
@@ -3,7 +3,11 @@
 # Dock
 defaults write com.apple.dock tilesize -int 36
 defaults write com.apple.dock autohide -bool true
-killall Dock
+
+# Restart Dock
+launchctl stop com.apple.Dock.agent
+sleep 0.1
+launchctl start com.apple.Dock.agent
 
 # Screenshots
 mkdir -p ~/Screenshots
