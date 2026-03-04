e1cf37b0d99bb7ce98a8175eae2c6e26e9bcd14f
Darren Clark <darrenjoelclark@icloud.com>
2026-03-04 15:49:41 -0500

    Disable predictive emoji picker on macOS

--- defaults.sh
+++ defaults.sh
@@ -9,6 +9,9 @@ launchctl stop com.apple.Dock.agent
 sleep 0.1
 launchctl start com.apple.Dock.agent
 
+# Disable annoying predictive emoji picker (reboot required)
+defaults write /Library/Preferences/FeatureFlags/Domain/UIKit.plist emoji_enhancements -dict-add Enabled -bool NO
+
 # Screenshots
 mkdir -p ~/Screenshots
 defaults write com.apple.screencapture location -string "~/Screenshots"
